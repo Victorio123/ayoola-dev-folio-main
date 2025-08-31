fa48018fa841ba42ad7341ec866b28786605efe3
Victorio123 <104024903+Victorio123@users.noreply.github.com>
2025-08-31 04:06:54 +0100

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,12 @@ import { ExternalLink, Github } from "lucide-react";
 
 const Projects = () => {
   const projects = [
+     {
+      title: "AgriTech AI webapp",
+      description: "Comprehensive web application for crop production and disease checking management with real-time analytics, API integration, and data-driven insights.",
+      link: "https://nextgen-breeders-alliance.vercel.app/",
+      github: "https://github.com/Victorio123/nextgen-breeders-hub", 
+    },
     {
       title: "New Ecom",
       description: "An e-commerce website for gadgets and fashion. Features smooth shopping experience, product listings, and intuitive navigation.",
